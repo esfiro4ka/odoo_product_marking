f0392a3bee186bb472c44612d6dedc83fa8e4be9
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2023-11-15 20:53:57 +0400

    add expense/revenue

--- addons/product_addition/models/product.py
+++ addons/product_addition/models/product.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Product(models.Model):
@@ -17,6 +17,48 @@ class Stock(models.Model):
     _description = 'Description for Stock'
 
     name = fields.Char(string='Stock Name', required=True)
+
+
+class ExpenseRevenueItem(models.Model):
+    _name = 'product_addition.expense_revenue_item'
+    _description = 'Expense/Revenue Item for Product'
+
+    purchase_cost = fields.Float(string='Purchase Cost')
+    logistics_cost = fields.Float(string='Logistics Cost')
+    promotion_expense = fields.Float(string='Promotion Expense')
+    agent_commission = fields.Float(string='Agent Commission')
+    sales_price = fields.Float(string='Sales Price')
+
+    marking_act_id = fields.Many2one(
+        'product_addition.marking_act', string='Marking Act')
+
+    @api.model
+    def _invert_value(self, field_name):
+        if getattr(self, field_name) >= 0:
+            setattr(self, field_name, -abs(getattr(self, field_name)))
+
+    @api.onchange('promotion_expense')
+    def _onchange_promotion_expense(self):
+        self._invert_value('promotion_expense')
+
+    @api.onchange('logistics_cost')
+    def _onchange_logistics_cost(self):
+        self._invert_value('logistics_cost')
+
+    @api.onchange('purchase_cost')
+    def _onchange_purchase_cost(self):
+        self._invert_value('purchase_cost')
+
+    @api.onchange('agent_commission')
+    def _onchange_agent_commission(self):
+        self._invert_value('agent_commission')
+
+
+class ExpenseRevenue(models.Model):
+    _name = 'product_addition.expense_revenue'
+    _description = 'Expense/Revenue for Product'
+
+    date = fields.Date(string='Date')
 
 
 class MarkedProduct(models.Model):
@@ -38,9 +80,6 @@ class MarkingAct(models.Model):
                        required=True, readonly=True, copy=False, default='New')
     date = fields.Date(string='Date',
                        default=fields.Date.today(), required=True)
-    # если разные товары в одном акте:
-    # product_lines = fields.Many2many('product_addition.product',
-    #                                  string='Product Lines')
     product_line = fields.Many2one('product_addition.product',
                                    string='Product', required=True)
     quantity = fields.Float(string='Quantity', default=1, required=True)
@@ -56,3 +95,8 @@ class MarkingAct(models.Model):
                                         string='Destination stock',
                                         create=True,
                                         required=True)
+    expense_revenue_item = fields.One2many(
+        'product_addition.expense_revenue_item',
+        'marking_act_id',
+        string='Expense/Revenue Items'
+    )
